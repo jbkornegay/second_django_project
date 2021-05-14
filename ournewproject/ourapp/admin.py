a464c96ecf669acdf19f158309255bf1f7ff7d77
Jarla Kornegay <jbkornegay@yahoo.com>
2021-05-13 22:55:10 -0400

    updated admin.py

--- ournewproject/ourapp/admin.py
+++ ournewproject/ourapp/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import School, Student
+from .models import School, Student, Certificate_Type, Grade, Faculty, Department
 
 # Register your models here.
 
 admin.site.register(School)
 admin.site.register(Student)
-
+admin.site.register(Certificate_Type)
+admin.site.register(Grade)
+admin.site.register(Faculty)
+admin.site.register(Department)
 
